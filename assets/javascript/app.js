6fda25a7d357e85f81ccc741a8f142061052a39b
Erin Stewart <dragonkittyrawr@users.noreply.github.com>
2017-06-01 12:11:59 -0500

    Single tweet text parsed out.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,74 @@
-var rantParse = [];
-var rantCharCount = [];
-var tweetCount;
-var wordCount = 0;
-var count;
+var twitter = {
+
+    rantParse: [],
+    rantCharCount: [],
+    tweetCount: 0,
+    wordCount: 0,
+    tweets: 0,
+    count: 1,
+
+    tweetParser: function(rantRaw, rantParse, rantCharCount) {
+        this.rantParse = rantParse;
+        this.rantCharCount = rantCharCount;
+        this.rantRaw = rantRaw;
+
+        if (rantCharCount.length > 99) {
+            tweetCount = Math.round(rantCharCount.length / 135);
+            console.log("tweetCount: " + tweetCount);
+        } else if (rantCharCount.length < 99 && rantCharCount.length > 9) {
+            tweetCount = (rantCharCount.length / 136);
+            console.log("tweetCount: " + tweetCount);
+        } else {
+            tweetCount = (rantCharCount.length / 137);
+            console.log("tweetCount: " + tweetCount);
+        };
+
+        // BUILD tweetCount
+
+        for (var p = 0; p < tweetCount; p++) {
+            tweets = p + 1;
+            console.log(tweets + "/" + tweetCount);
+        };
+
+        // BUILD wordCount
+
+        for (var i = 0; i < rantParse.length; i++) {
+            // console.log("rantParse[i].length: " + rantParse[i].length);
+            this.wordCount += rantParse[i].length + 1;
+            console.log("wordCount: " + this.wordCount);
+
+
+            if (this.wordCount < 140) {
+
+                window["tweet" + this.count].push(this.rantParse[i]);
+                console.log(this.rantParse[i]);
+                console.log("Tweet " + this.count + ": ");
+                console.log(window["tweet" + this.count]);
+
+            }
+        };
+    }
+};
+
+window["tweet" + twitter.count] = [];
+
+// GET RANT
 
 $("#rantSubmit").on("click", function(event) {
-    // Prevent page refresh.
+    // PREVENT PAGE REFRESH
     event.preventDefault();
+
+    // CREATE rantRaw
     rantRaw = $("#rantRaw").val();
-    console.log(rantRaw);
+    console.log("rantRaw: " + rantRaw);
+
+    // CREATE rantParse
     rantParse = rantRaw.split(" ");
-    console.log(rantParse);
+    console.log("rantParse: " + rantParse);
+
+    // CREATE rantCharCount
     rantCharCount = rantRaw.split("");
-    console.log(rantCharCount.length);
+    console.log("rantCharCount: " + rantCharCount.length);
 
-
-
-    tweetParser(rantRaw, rantParse, rantCharCount);
+    twitter.tweetParser(rantRaw, rantParse, rantCharCount);
 });
-
-function tweetParser(rantRaw, rantParse, rantCharCount) {
-
-
-    if (rantCharCount.length > 99) {
-        tweetCount = Math.round(rantCharCount.length / 135);
-        console.log(tweetCount);
-    } else if (rantCharCount.length < 99 && rantCharCount.length > 9) {
-        tweetCount = (rantCharCount.length / 136);
-        console.log(tweetCount);
-    } else {
-        tweetCount = (rantCharCount.length / 137);
-        console.log(tweetCount);
-    };
-
-    // BUILD TWEETCOUNT
-
-    for (var p = 0; p < tweetCount; p++) {
-        count = p + 1;
-        console.log(count + "/" + tweetCount);
-    }
-
-    // BUILD WORDCOUNT
-
-    for (var i = 0; i < rantParse.length; i++) {
-        console.log(rantParse[i].length);
-        wordCount += rantParse[i].length + 1;
-        console.log(wordCount);
-
-        if (wordCount < 140) {
-            window["tweet" + count] = [];
-            window["tweet" + count].push(rantParse[i]);
-            
-        }
-    }
-
-    console.log("Tweet " + count + ": " + window["tweet" + count]);
-
-
-};
